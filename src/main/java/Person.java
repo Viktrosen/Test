10ca7db6ba9a706c4297c570e1f32363823acd0a
Viktrosen <bolotov.vit@yandex.ru>
2019-11-02 13:51:03 +0500

    Создал класс, который будет обозначать одного отдельно взятого человека и его данные

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,4 +1,48 @@
 
 public class Person {
-    
+   private String name;
+    private String surname;
+    private int age;
+    private int number;
+    private int balance;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
 }

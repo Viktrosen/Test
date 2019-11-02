f384bf15138f267309c97a2897de808e985e54e3
Viktrosen <bolotov.vit@yandex.ru>
2019-11-02 14:25:00 +0500

    Добавил несколько полей, настроил генерацию полей Balance и Number

--- src/main/java/Generator.java
+++ src/main/java/Generator.java
@@ -1,9 +1,17 @@
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class Generator {
+    @Autowired
+Person person;
+List<Person> list;
 
     void gen(){
+person.setBalance((int)(Math.random()*20+3));
+person.setNumber((long)(Math.random()*500000000+1000000000));
 
 
     }
